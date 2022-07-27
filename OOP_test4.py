3f88f26dfdcbb30da425b74d8d803a2accb50568
Kiryl Zholudzeu <kirillzholudev24@gamil.com>
2022-07-27 14:05:38 +0200

    Got the solution right. (cube)

--- OOP_test4.py
+++ OOP_test4.py
@@ -2,25 +2,20 @@ from random import randint, randrange
 
 
 class Cube:
-    edge = 0
-    b = []
+
+    def __init__(self, edge):
+        self.edge = edge
 
     def throw(self):
         print(f'You have a {self.edge} grain cube')
 
-        for i in range(1, self.edge + 1):
-            self.b.append(i)
-
-        my_range = randrange(len(self.b))
+        my_range = randrange(1, self.edge, 1)
         print(f'After the roll, you dropped a edge with the number {my_range}\n')
 
 # Cube 1
-throw1 = Cube()
-throw1.edge = randint(6, 12)
-throw1.throw()
+cube1 = Cube(edge=6)
+cube1.throw()
 
 # Cube 2
-throw2 = Cube()
-throw2.edge = randint(6, 12)
-throw2.throw()
-
+cube2 = Cube(edge=12)
+cube2.throw()
