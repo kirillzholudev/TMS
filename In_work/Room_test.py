118822b37f8f3773eb30bfc18e7b434d3eeefdfc
Kiryl <kirillzholudev24@gmail.com>
2022-08-14 12:36:15 +0200

    update Room_test.py

--- In_work/Room_test.py
+++ In_work/Room_test.py
@@ -1,42 +1,29 @@
 from math import ceil
 
-sides = {"Length": 0,
-         "Width": 0,
-         "Height": 0}
-
 
 class WinDoor:
-    def __init__(self, x, y, name="unk"):
-        self.x = x
-        self.y = y
+    def __init__(self, height, width, name: str):
+        self.width = width
+        self.height = height
         self.name = name
-        self.square = x * y
+        self.square = height * width
 
     def __repr__(self):
-        return f'{self.name} {self.x}x{self.y}'
+        return f'{self.name} {self.height}height{self.width}'
 
 
 class Room:
-    def __init__(self, x, y, z):
-        self.square = 2 * z * (x + y)
-        self.new_square = 0
+    def __init__(self, height, width_r, length_r):
+        self.total_roll = None
+        self.square = 2 * height * (width_r + length_r)
+        self.new_square = None
         self.wd = []
-        self.sides = sides
-        self.x = x
-        self.y = y
-        self.z = z
-        self.printSides()
+        self.width_r = width_r
+        self.length_r = length_r
+        self.height = height
 
-    def printSides(self):
-        sides['Length'] = self.x
-        sides['Width'] = self.y
-        sides['Height'] = self.z
-        print('Room size:\n')
-        for key, value in sides.items():
-            print("{0}: {1}".format(key, value))
-
-    def addWD(self, w, h, name='unk'):
-        self.wd.append(WinDoor(w, h, name))
+    def addWD(self, height, width, name: str):
+        self.wd.append(WinDoor(height, width, name))
 
     def workSurface(self):
         self.new_square = self.square
@@ -45,47 +32,66 @@ class Room:
         return self.new_square
 
     def wallpaper(self, dl, sh):  # Length and width of the roll
-        roll_square = dl * sh
-        total_roll = self.new_square / roll_square
-        return f'You need {ceil(total_roll)} roll'
+        self.total_roll = self.new_square / (dl * sh)
+        return ceil(self.total_roll)
 
 
-"""Program interface"""
+class Interface:
+    def __init__(self):
+        self.room = Room(
+            float(input("Enter Height of room: ")),
+            float(input("Enter Width of room: ")),
+            float(input("Enter Length of room: "))
+        )
+        self.addwindoor = self.windoor()
+        self.square = self.room.workSurface()
+        self.roll_wallpaper = self.roll()
 
-print('Add the room parameters first')
-ch1 = float(input("Length of room: "))
-ch2 = float(input("Width of room: "))
-ch3 = float(input("Height of room: "))
-room_1 = Room(ch1, ch2, ch3)
-print("Square of room: ", room_1.square)
+    def windoor(self):
+        ch_1 = str(input('Do you have a window or door? Y/N\n'
+                         '--> '))
+        work_1 = True
+        while work_1:
+            if ch_1 == 'Y' or 'y':
+                i = input('1. Add window\n'
+                          '2. Add door\n'
+                          '3. Next step\n'
+                          '--> ')
+                count_w = 0
+                count_d = 0
+                if i == '1':
+                    count_w += 1
+                    self.room.addWD(
+                        float(input(f"Window {count_w}. Enter the window height: ")),
+                        float(input("\t\tEnter the window width: ")),
+                        input("\t\tEnter the window name: ")
+                    )
+                    continue
+                if i == '2':
+                    count_d += 1
+                    self.room.addWD(
+                        float(input(f"Door {count_d}. Enter the door height: ")),
+                        float(input("\t\tEnter the door width: ")),
+                        input("\t\tEnter the door name: ")
+                    )
+                    continue
+                else:
+                    work_1 = False
+            else:
+                break
 
-while True:
-    print("CHOICES:\n"
-          "1. Add Windows or Doors\n"
-          "2. Change sides of the room\n"
-          "3. Calculate the amount of wallpaper")
-    choice = input()
+    def roll(self):
+        return self.room.wallpaper(
+            float(input("Enter the length of the wallpaper: ")),
+            float(input("Enter the width of the wallpaper: "))
+        )
 
-    if choice == '1':
-        w = float(input("Width of door or window: "))
-        h = float(input("Height of door or window: "))
-        name = input("Name of door or window: ")
-        room_1.addWD(w, h, name)
-        print(room_1.wd)
-        print('Work area: ', room_1.workSurface())
-        continue
+    def __repr__(self):
+        return (
+            f"\nYour work surface: {self.square} "
+            f"\nYou need: {self.roll_wallpaper}, roll(s) of wallpaper"
+        )
 
-    if choice == '2':
-        print("*** *** ***\n "
-              "This part of the programme is still not working."
-              "\n*** *** ***\n")
-        continue
 
-    if choice == '3':
-        print('The area to wallpaper:', room_1.workSurface())
-        dl = float(input('Roll length '))
-        sh = float(input('Roll width '))
-        print(room_1.wallpaper(dl, sh))
-        break
-    else:
-        break
+start_interface = Interface()
+print(start_interface)
